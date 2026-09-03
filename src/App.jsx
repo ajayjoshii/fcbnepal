cde9c6ab838e48095c8b320d4d4859932325dc9f
cloudswebnepal8 <cloudswebnepal8@gmail.com>
2026-09-03 09:41:24 +0545

    files

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 // import Home from './pages/Home'
 import Navbar from './components/Navbar';
-// import AboutHero from './pages/About/Leaders';
+import AboutHero from './pages/About/Leaders';
 import Footer from './components/Footer';
-// import Contact from './pages/Contact';
-// import Statementfaith from './pages/About/Statementfaith';
-// import Church_CovenantParent from './components/Church_Covenant/Church_CovenantParent';
+import Contact from './pages/Contact';
+import Statementfaith from './pages/About/Statementfaith';
+import Church_CovenantParent from './components/Church_Covenant/Church_CovenantParent';
 import ScrollToTop from './components/ScrollToTop';
-// import Ladies_Bible from './pages/Ministries/Ladies_Bible';
-// import Mens_Bible from './pages/Ministries/Mens_Bible';
-// import Pastoral from './pages/Ministries/Pastoral';
-// import Sunday from './pages/Ministries/Sunday';
-// import Sermons from './pages/Sermons';
+import Ladies_Bible from './pages/Ministries/Ladies_Bible';
+import Mens_Bible from './pages/Ministries/Mens_Bible';
+import Pastoral from './pages/Ministries/Pastoral';
+import Sunday from './pages/Ministries/Sunday';
+import Sermons from './pages/Sermons';
 
 function App() {
   return (
     <>
       <Navbar />
       <ScrollToTop/>
-      {/* <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about/our-leaders' element={<AboutHero />} />
+       <Routes>
+        {/* <Route path='/' element={<Home />} /> */}
+         <Route path='/about/our-leaders' element={<AboutHero />} />
         <Route path='/plan-a-visit' element={<Contact />} />
         <Route path='/about/statement-of-faith' element={<Statementfaith />} />
         <Route path='/about/church-covenant' element={<Church_CovenantParent />} />
@@ -30,7 +30,7 @@ function App() {
         <Route path='/ministries/pastoral-internship' element={<Pastoral/>} />
         <Route path='/ministries/sunday-school' element={<Sunday/>} />
         <Route path='/sermons' element={<Sermons/>} />
-      </Routes> */}
+      </Routes>
       <Footer />
 
     </>
