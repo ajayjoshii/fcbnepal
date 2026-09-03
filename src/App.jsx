e9d6c2c052a514c50ec065a8f1829f3774715d67
cloudswebnepal8 <cloudswebnepal8@gmail.com>
2026-09-03 09:45:42 +0545

    files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-// import Home from './pages/Home'
+import Home from './pages/Home'
 import Navbar from './components/Navbar';
 import AboutHero from './pages/About/Leaders';
 import Footer from './components/Footer';
